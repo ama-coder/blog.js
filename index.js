0bdc5368b89913fddfbde6404a8a4f06ae1e716c
AmACoder <amacoder88@gmail.com>
2021-09-18 18:31:07 +0430

    add ajax func

--- index.js
+++ index.js
@@ -3,34 +3,21 @@
 
 var xhttp = new XMLHttpRequest();
 
-var getPostsInfo = (path) => {
+var getdata = (path) => {
 
     xhttp.open('GET', path, false);
     xhttp.send(null);
     return JSON.parse(xhttp.responseText);
 
 }
+var getPostsInfo = (path) => {
+
+
+
+}
 
 var getPost = (postname,postsInfo) => {
 
-    let postPath;
+    
 
-    console.log(postsInfo.count);
-
-    if (postsInfo == null) {
-        console.error("postes info not fetcged (try getPostsInfo())");
-        return;
-    }
-    for (var i=0; i<=postsInfo.count;i++)
-    { 
-        if(postname == postsInfo.posts[i]) {
-            postPath = postsInfo.path[i];
-            return postPath;
-           // break;
-        }
-    }
-    // for (var i in postsInfo.count) 
-    // {
-       
-    // }
 }
